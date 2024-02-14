2ca0dd70b732297eccfc154944b2a52b8d0a59b2
FridaWikman <frida.wikman@hotmail.com>
2024-02-14 10:12:19 +0100

    Installerat router och Lagt till routerviews

---
+++ src/router.js
@@ -0,0 +1,33 @@
+import {
+  createRouter, createWebHashHistory
+} from 'vue-router'
+
+import HomeView from './views/HomeView.vue'
+import ProductsView from './views/ProductsView.vue'
+import ProductView from './views/ProductView.vue'
+
+
+
+export default createRouter ({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      component: HomeView,
+      path: '/'
+    },
+    {
+      component: ProductsView,
+      path: '/products'
+    },
+    {
+      component: ProductView,
+      path: '/product'
+      // adressparameter
+    },
+    // {
+    //   component: ShoppingcartView,
+    //   path: '/shoppingcart'
+    // },
+
+  ]
+})
