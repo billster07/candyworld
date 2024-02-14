803aa5ea0c3508550520a623a8afc38650206e9f
Will <williamhermanrud@hotmail.com>
2024-02-14 15:03:48 +0100

    Utforska produkter påbörjad

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+import "./style.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
