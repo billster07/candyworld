fa59380fe547b7b911c7a5721df56041aeb6bc10
FridaWikman <frida.wikman@hotmail.com>
2024-02-15 19:24:04 +0100

    Påbörjat footern

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,40 @@
 <script setup>
+import { ref } from 'vue'
+
+const footerWave = ref('footer-transparent.png'),
+  smallLogo = ref('small-logo-1.png')
 </script>
 
 
 <template>
+  <div class="imageContainer">
+    <img class="img1" :src="footerWave" alt="Footer">
+    <img class="img2" :src="smallLogo" alt="Logo">
+  </div>
 </template>
 
 
 <style scoped>
+img {
+  max-width: 100%;
+  background-color: rgba(231, 182, 226, 0.46);
+}
+
+.imageContainer {
+  position: relative;
+  top: 0%;
+  left: 0%;
+}
+
+.img1 {
+  position: relative;
+  top: 0%;
+  left: 0%;
+}
+
+.img2 {
+  position: absolute;
+  top: 40px;
+  right: 30px;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,38 @@
 <script setup>
- import {ref} from 'vue'
-    const candyPicture1 = ref ("candyPhoto1.png"),
-     candyPicture2 = ref ("candyPhoto2.png")
+import { ref } from 'vue'
+import Footer from '../components/Footer.vue'
 
-    
+const candyPicture1 = ref("candyPhoto1.png"),
+  candyPicture2 = ref("candyPhoto2.png")
+
+
 </script>
 <template>
-    <div class="pContainer">
-  <p class="introDescription">Här har vi samlat godis och läckerheter från olika delar av världen.
-Upptäck det senaste från TikTok eller hitta din favorit från ditt land, vi har något för alla!</p>
-</div>
-<img class="candyPhoto" alt="Candy Photo" v-bind:src="candyPicture1">
-<!-- karusell med bilder läggs in här -->
-<div class="yellowBoxMediaQuery">
+  <div class="pContainer">
+    <p class="introDescription">Här har vi samlat godis och läckerheter från olika delar av världen.
+      Upptäck det senaste från TikTok eller hitta din favorit från ditt land, vi har något för alla!</p>
+  </div>
+  <img class="candyPhoto" alt="Candy Photo" v-bind:src="candyPicture1">
+  <!-- karusell med bilder läggs in här -->
+  <div class="yellowBoxMediaQuery">
     <!-- flex wrap för desktop -->
-<div class="yellowBox">
-<p class="yellowBoxDescription">Utforska en värld av läckerheter, där varje tugga är en resa.
-Candyworld tar dig med på en global smakupplevelse, ett exotiskt godis i taget. Gör din smakresa komplett och låt Candyworld vara din guide till spännande smaker från alla hörn av världen. </p>
-<b-button class="button" size="lg">Se produkter</b-button>
-</div>
-<img class="candyPhoto" alt="Candy Photo" v-bind:src="candyPicture2">
-</div>
+    <div class="yellowBox">
+      <p class="yellowBoxDescription">Utforska en värld av läckerheter, där varje tugga är en resa.
+        Candyworld tar dig med på en global smakupplevelse, ett exotiskt godis i taget. Gör din smakresa komplett och låt
+        Candyworld vara din guide till spännande smaker från alla hörn av världen. </p>
+      <b-button class="button" size="lg">Se produkter</b-button>
+    </div>
+    <img class="candyPhoto" alt="Candy Photo" v-bind:src="candyPicture2">
+  </div>
+  <Footer></Footer>
 </template>
 
 <style scoped>
-
 .pContainer {
-    margin-top: 25px;
-    margin-bottom: 20px;
+  margin-top: 25px;
+  margin-bottom: 20px;
 }
+
 .introDescription {
   margin-left: 30px;
   margin-right: 30px;
@@ -36,20 +40,21 @@ Candyworld tar dig med på en global smakupplevelse, ett exotiskt godis i taget.
 }
 
 .candyPhoto {
-    max-width: 100%;
+  max-width: 100%;
 }
+
 .yellowBox {
-    background-color: #FFE67B;
-    display: flex;
-   flex-direction: column;
+  background-color: #FFE67B;
+  display: flex;
+  flex-direction: column;
 }
 
 .yellowBoxDescription {
-    text-align: center;
-    margin-left: 30px;
-    margin-right: 30px;
-    margin-top: 40px;
-    margin-bottom: 45px;
+  text-align: center;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 40px;
+  margin-bottom: 45px;
 }
 
 .button {
@@ -62,6 +67,4 @@ Candyworld tar dig med på en global smakupplevelse, ett exotiskt godis i taget.
   align-self: center;
   margin-bottom: 2em;
 }
-
 </style>
-
