5cfd08188115ee612b75e574324856663f991b33
FridaWikman <frida.wikman@hotmail.com>
2024-02-18 18:19:51 +0100

    Lagt till startsida, med klickbar knapp.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,100 +7,91 @@ const candyPicture1 = ref("candyPicture500.png"),
 
 </script>
 <template>
-  <div class="pContainer">
-
-    <p class="introDescription">Här har vi samlat godis och läckerheter från olika delar av världen.
-      Upptäck det senaste från TikTok eller hitta din favorit från ditt land, vi har något för alla!</p>
-
-    <div class="photoContainer">
-
-      <b-img fluid-grow class="candyPhoto" alt="Candy Photo" v-bind:src="candyPicture1"></b-img>
+  <div class="wrapPAndImage1">
+    <div class="yellowBox">
+      <p class="yellowBoxDescription">Här har vi samlat godis och läckerheter från olika delar av världen.
+        Upptäck det senaste från TikTok eller hitta din favorit från ditt land, vi har något för alla!</p>
     </div>
+    <b-img fluid-grow class="candyPhoto" alt="Candy Photo" :src="candyPicture1"></b-img>
   </div>
 
-  <!-- karusell med bilder läggs in här
-  <div class="yellowBoxMediaQuery"> </div> -->
-  <!-- flex wrap för desktop -->
-  <div class="wrapPAndImage">
+
+  <!-- karusell med bilder läggs in här -->
+
+  <div class="wrapPAndImage2">
     <div class="yellowBox">
       <p class="yellowBoxDescription">Utforska en värld av läckerheter, där varje tugga är en resa.
         Candyworld tar dig med på en global smakupplevelse, ett exotiskt godis i taget. Gör din smakresa komplett och låt
         Candyworld vara din guide till spännande smaker från alla hörn av världen. </p>
-      <b-button class="button" size="lg">Se produkter</b-button>
+      <b-button @click="$router.push('/products')" class="button" size="lg">Se produkter</b-button>
     </div>
-    <b-img fluid-grow class="candyPhoto2" alt="Candy Photo" v-bind:src="candyPicture2"></b-img>
+    <b-img fluid-grow class="candyPhoto" alt="Candy Photo" :src="candyPicture2"></b-img>
   </div>
+
+  <!-- karusell med bilder läggs in här -->
 </template>
 
 <style scoped>
-.pContainer {
-  align-content: center;
-  display: flex;
-  flex-direction: column;
-  margin-top: 25px;
-  margin-bottom: 20px;
-  /* max-width: 614px; */
+.wrapPAndImage1 {
+  background-color: #FFE67B;
 
 }
 
-.introDescription {
-  align-self: center;
-  margin-left: 30px;
-  margin-right: 30px;
-  text-align: center;
-  max-width: 350px;
-
+.wrapPAndImage2 {
+  background-color: #FFE67B;
 }
 
-
-
-/* .photoContainer {
-  display: flex;
-  align-items: center;
-} */
-
-/* max-width: 700px; */
-/* align-self: center; */
 
 .yellowBox {
-  background-color: #FFE67B;
+  align-content: center;
   display: flex;
   flex-direction: column;
-  align-content: center;
+  justify-content: center;
 }
 
 .yellowBoxDescription {
-  text-align: center;
   align-self: center;
+  margin-bottom: 45px;
   margin-left: 30px;
   margin-right: 30px;
   margin-top: 40px;
-  margin-bottom: 45px;
   max-width: 350px;
+  text-align: center;
 }
 
 .button {
+  align-self: center;
   background-color: #E7B6E2;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  border-radius: 0.75rem;
   border: 0;
+  border-radius: 0.75rem;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  margin-bottom: 2em;
   padding: 5px 15px 5px 15px;
   width: 8em;
-  align-self: center;
-  margin-bottom: 2em;
 }
 
-@media (min-width: 600px) {
-  .wrapPAndImage {
+
+@media (min-width: 670px) {
+  .wrapPAndImage1 {
     display: flex;
+    min-height: 400px;
+  }
+
+  .wrapPAndImage2 {
+    display: flex;
+    flex-direction: row-reverse;
+    min-height: 400px;
   }
 
   .yellowBox {
     min-width: 50%;
   }
 
-  .candyPhoto2 {
-    min-width: 50%;
+  .candyPhoto {
+    margin: 60px;
+    max-height: 300px;
+    max-width: 300px;
   }
+
 }
 </style>
