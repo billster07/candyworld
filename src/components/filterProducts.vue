0ddd65c05ad827b23349b9e2cc5d16c189aa9281
Will <williamhermanrud@hotmail.com>
2024-02-19 16:04:52 +0100

    påbörjat enskild produktsida

--- src/components/filterProducts.vue
+++ src/components/filterProducts.vue
@@ -5,59 +5,52 @@ import { useCandyStore } from "/src/store.js";
 
 const store = useCandyStore();
 store.fetchProducts();
-const props = defineProps({filterCategory: {type: String}})
+const props = defineProps({ filterCategory: { type: String } })
 </script>
 
 <template>
   <div class="productContainer">
-    <b-container v-if="props.filterCategory === 'Alla'" class="productCard" v-for="product in store.products">
-    <b-row align-v="center">
-      <b-col class="image">
-        <img
-          :src="
-            'https://pb.nopatan.com/api/files/02eld6u8qdz3cgq/' +
+    <b-container @click="store.matchProduct(product.id), $router.push('/product')" v-if="props.filterCategory === 'Alla'"
+      class="productCard" v-for="product in store.products">
+      <b-row align-v="center">
+        <b-col class="image">
+          <img :src="'https://pb.nopatan.com/api/files/02eld6u8qdz3cgq/' +
             product.id +
             '/' +
             product.image
-          "
-        />
-      </b-col>
-      <b-col cols="8">
-        <h3>{{ product.productName }}</h3>
-        <p> {{ product.description_sum }}</p>
-        <div class="priceButtonDesign">
-          <p>{{ product.price }}:-</p>
-          <b-button class="button" size="sm"
-            >KÖP <i class="bi bi-cart"></i></b-button>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
-    <b-container v-else class="productCard" v-for="product in store.products.filter(product => product.category === props.filterCategory)">
-    <b-row align-v="center">
-      <b-col class="image">
-        <img
-          :src="
-            'https://pb.nopatan.com/api/files/02eld6u8qdz3cgq/' +
+            " />
+        </b-col>
+        <b-col cols="8">
+          <h3>{{ product.productName }}</h3>
+          <p> {{ product.description_sum }}</p>
+          <div class="priceButtonDesign">
+            <p>{{ product.price }}:-</p>
+            <b-button class="button" size="sm">KÖP <i class="bi bi-cart"></i></b-button>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container @click="store.matchProduct(product.id), $router.push('/product')" v-else class="productCard"
+      v-for="product in store.products.filter(product => product.category === props.filterCategory)">
+      <b-row align-v="center">
+        <b-col class="image">
+          <img :src="'https://pb.nopatan.com/api/files/02eld6u8qdz3cgq/' +
             product.id +
             '/' +
             product.image
-          "
-        />
-      </b-col>
-      <b-col cols="8">
-        <h3>{{ product.productName }}</h3>
-        <p> {{ product.description_sum }}</p>
-        <div class="priceButtonDesign">
-          <p>{{ product.price }}:-</p>
-          <b-button class="button" size="sm"
-            >KÖP<i class="bi bi-cart"></i
-          ></b-button>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
-</div>
+            " />
+        </b-col>
+        <b-col cols="8">
+          <h3>{{ product.productName }}</h3>
+          <p> {{ product.description_sum }}</p>
+          <div class="priceButtonDesign">
+            <p>{{ product.price }}:-</p>
+            <b-button class="button" size="sm">KÖP<i class="bi bi-cart"></i></b-button>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <style scoped>
@@ -111,16 +104,16 @@ img {
 @media (min-width: 500px) and (max-width: 699px) {
 
   .productCard {
-  margin-top: 15px;
-  padding-top: 10px;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  font-size: 16px;
+    margin-top: 15px;
+    padding-top: 10px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    font-size: 16px;
   }
 
   img {
-  max-width: 120px;
-  max-height: 120px;
-  margin-left: 15px;
+    max-width: 120px;
+    max-height: 120px;
+    margin-left: 15px;
   }
 
 }
@@ -134,17 +127,18 @@ img {
   /* testade att lägga en <div class= "productContainer"> runt hela innehållet och göra det till en flexbox, men verkar som att bootstrap? overridar det på nåt sätt? */
 
   .productCard {
-  /* width: 50%; */
-   /* detta borde vara det som krävs för att 2 kort läggs sida vid sida i flexboxen. funkar ej?  */
-  margin-top: 15px;
-  padding-top: 30px;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  font-size: 17px;
+    /* width: 50%; */
+    /* detta borde vara det som krävs för att 2 kort läggs sida vid sida i flexboxen. funkar ej?  */
+    margin-top: 15px;
+    padding-top: 30px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    font-size: 17px;
   }
+
   img {
-  width: 120px;
-  height: 120px;
-  margin-left: 20px;
+    width: 120px;
+    height: 120px;
+    margin-left: 20px;
   }
 
 
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,2 +1,50 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { useCandyStore } from "/src/store.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const store = useCandyStore();
+console.log(store.selectedProduct)
+</script>
+
+<template>
+  <div class="product">
+    <div class="image">
+      <img :src="'https://pb.nopatan.com/api/files/02eld6u8qdz3cgq/' +
+        store.selectedProduct.id +
+        '/' +
+        store.selectedProduct.image
+        ">
+    </div>
+    <div class="price-status">
+      <p>{{ store.selectedProduct.price }}:-</p>
+      <p><i class="bi bi-circle-fill" variant="success"></i> I lager</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.product {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.image {
+  width: 50%;
+}
+
+img {
+  width: 100%;
+  max-height: auto;
+}
+
+/* i {
+  color: green;
+} */
+.price-status {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
