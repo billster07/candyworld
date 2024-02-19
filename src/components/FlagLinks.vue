81c75f1591bf2e0edaa85a42524385503800a17e
FridaTheDev <105454838+FridaTheDev@users.noreply.github.com>
2024-02-19 16:52:01 +0100

    Komponent för flaggorna skapad

---
+++ src/components/FlagLinks.vue
@@ -0,0 +1,38 @@
+<script setup>
+import filterProducts from "../components/filterProducts.vue";
+import { useRouter, useRoute } from "vue-router";
+import { ref, watch } from "vue";
+
+
+
+const router = useRouter();
+
+const navigateToCountry = (category) => {
+  router.push({ params: { category } });
+};
+
+
+
+
+</script>
+<template>
+  <div class="flag-container">
+    <img src="/Balkan.png" alt="" @click="navigateToCountry('Balkan')" />
+    <img src="/japan.png" alt=""  @click="navigateToCountry('Japan')"/>
+    <img src="/Mexico.png" alt=""  @click="navigateToCountry('Mexico')"/>
+    <img src="/Turkey.png" alt="" @click="navigateToCountry('Turkiet')"/>
+    <img src="/USA.png" alt="" @click="navigateToCountry('USA')"/>
+    <img src="/UK.png" alt="" @click="navigateToCountry('Storbritannien')"/>
+  </div>
+
+
+</template>
+
+<style>
+.flag-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-bottom: 10px;
+}
+</style>
