33a4d5964b83e9ab588aa6c86587d28ec0168f7d
FridaTheDev <105454838+FridaTheDev@users.noreply.github.com>
2024-03-04 14:36:18 +0100

    Lagt till loading funktion och utökat funktioner i formulären

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -1,32 +1,37 @@
 <script setup>
-
 import { useRouter } from "vue-router";
 const router = useRouter();
 
 // function navigateToHome() {
 //   router.push('./views/HomeView')
 // } gjorde med router-link istället? Detta är onödigt komplicerat?
-
-
 </script>
 
 <template>
-<div class="confirmationContainer">
-  <h1>Ditt köp har genomförts!</h1>
-  <i class="bi bi-check-circle"></i>
-  <div class="confirmationText">
-    <h2> Tack för ditt köp!</h2>
-    <p>Ditt ordernummer är <b>2348526</b>.</p>
-    <p>Beräknad leveranstid <b> Måndag 17 Februari (moments). </b></p>
-    <p>Din orderbekräftelse skickas till <b>sara@gmail.com</b> textinterpolering</p>
-    <p>För att ändra i din order, följ instruktionerna i orderbekräftelse.</p>
+  <div class="confirmationContainer">
+    <h1>Ditt köp har genomförts! <i class="bi bi-check-circle"></i></h1>
+    <div class="confirmationText">
+      <h2>Tack för ditt köp!</h2>
+      <p>Ditt ordernummer är <b>2348526</b>.</p>
+      <p>Beräknad leveranstid <b> Måndag 17 Februari (moments). </b></p>
+      <p>
+        Din orderbekräftelse skickas till
+        <b>sara@gmail.com</b> textinterpolering
+      </p>
+      <p>För att ändra i din order, följ instruktionerna i orderbekräftelse.</p>
+    </div>
+    <router-link to="/"
+      ><b-button type="text" variant="primary" class="homeButton"
+        >Till Startsidan</b-button
+      ></router-link
+    >
   </div>
-  <router-link to="/"><b-button type="text" variant="primary" class="homeButton">Till Startsidan</b-button></router-link>
-</div>
-
 </template>
 
 <style scoped>
+.bi {
+  font-size: 40px;
+}
 
 .homeButton {
   margin-top: 3rem;
@@ -38,11 +43,11 @@ const router = useRouter();
   padding-right: 30px;
 }
 
-.homeButton:hover{
-  background-color:rgba(255, 164, 85, 0.8);
+.homeButton:hover {
+  background-color: rgba(255, 164, 85, 0.8);
 }
 
-.confirmationContainer{
+.confirmationContainer {
   display: flex columns;
   justify-content: center;
   max-width: 60%;
@@ -54,7 +59,4 @@ const router = useRouter();
   padding: 10px;
   background-color: #e7b6e269;
 }
-
-
-
 </style>
